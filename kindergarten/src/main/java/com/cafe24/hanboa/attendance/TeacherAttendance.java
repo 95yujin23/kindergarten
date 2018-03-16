2108cb85aded8bc956b96dc78d8cd3a14feaea1c
김규원 <kyuwonism@gmail.com>
2018-03-16 14:06:41 +0900

    김규원 - TeacherAttendance.java 파일 생성

---
+++ kindergarten/src/main/java/com/cafe24/hanboa/attendance/TeacherAttendance.java
@@ -0,0 +1,52 @@
+package com.cafe24.hanboa.attendance;
+
+public class TeacherAttendance {
+	//교직원근태 tb_teacher_attendance
+	
+	private String attendanceCd;
+	private String teacherCd;
+	private String teacherAttendance;
+	private String teacherGetOff;
+	private String teacherRestLateType;
+	private String licenseKindergarten;
+	
+	public String getAttendanceCd() {
+		return attendanceCd;
+	}
+	public void setAttendanceCd(String attendanceCd) {
+		this.attendanceCd = attendanceCd;
+	}
+	public String getTeacherCd() {
+		return teacherCd;
+	}
+	public void setTeacherCd(String teacherCd) {
+		this.teacherCd = teacherCd;
+	}
+	public String getTeacherAttendance() {
+		return teacherAttendance;
+	}
+	public void setTeacherAttendance(String teacherAttendance) {
+		this.teacherAttendance = teacherAttendance;
+	}
+	public String getTeacherGetOff() {
+		return teacherGetOff;
+	}
+	public void setTeacherGetOff(String teacherGetOff) {
+		this.teacherGetOff = teacherGetOff;
+	}
+	public String getTeacherRestLateType() {
+		return teacherRestLateType;
+	}
+	public void setTeacherRestLateType(String teacherRestLateType) {
+		this.teacherRestLateType = teacherRestLateType;
+	}
+	public String getLicenseKindergarten() {
+		return licenseKindergarten;
+	}
+	public void setLicenseKindergarten(String licenseKindergarten) {
+		this.licenseKindergarten = licenseKindergarten;
+	}
+	
+	
+	
+}
