7517d18232f583d91a469b9bc159c8155907c46b
김규원 <kyuwonism@gmail.com>
2018-03-16 12:17:35 +0900

    김규원 - ClassController.java / ClassDao.java / ClassMapper.java / ClassService.java 클래스 파일 생성

---
+++ kindergarten/src/main/java/com/cafe24/hanboa/classformation/ClassController.java
@@ -0,0 +1,6 @@
+package com.cafe24.hanboa.classformation;
+
+public class ClassController {
+	//반 관련 컨트롤러
+
+}
---
+++ kindergarten/src/main/java/com/cafe24/hanboa/classformation/ClassService.java
@@ -0,0 +1,5 @@
+package com.cafe24.hanboa.classformation;
+
+public class ClassService {
+	// 반 편성 처리 service
+}
