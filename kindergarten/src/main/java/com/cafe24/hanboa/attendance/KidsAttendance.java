3e35ae5a345710f3b9c7460d2f642090172207f8
김규원 <kyuwonism@gmail.com>
2018-03-16 13:50:18 +0900

    김규원 - com.cafe24.hanboa.attendance 패키지 생성 / com.cafe24.hanboa.attendance 패키지 내 KidsAttendance.java 생성

---
+++ kindergarten/src/main/java/com/cafe24/hanboa/attendance/KidsAttendance.java
@@ -0,0 +1,58 @@
+package com.cafe24.hanboa.attendance;
+
+public class KidsAttendance {
+	//영유아등하원 tb_kids_attendance
+	
+	private String attendanceCd;
+	private String kidsCd;
+	private String kidsAttendanceTime;
+	private String kidsComeHomeTime;
+	private String kidsRestLateTime;
+	private String kidsReason;
+	private String licenseKindergarten;
+	
+	public String getAttendanceCd() {
+		return attendanceCd;
+	}
+	public void setAttendanceCd(String attendanceCd) {
+		this.attendanceCd = attendanceCd;
+	}
+	public String getKidsCd() {
+		return kidsCd;
+	}
+	public void setKidsCd(String kidsCd) {
+		this.kidsCd = kidsCd;
+	}
+	public String getKidsAttendanceTime() {
+		return kidsAttendanceTime;
+	}
+	public void setKidsAttendanceTime(String kidsAttendanceTime) {
+		this.kidsAttendanceTime = kidsAttendanceTime;
+	}
+	public String getKidsComeHomeTime() {
+		return kidsComeHomeTime;
+	}
+	public void setKidsComeHomeTime(String kidsComeHomeTime) {
+		this.kidsComeHomeTime = kidsComeHomeTime;
+	}
+	public String getKidsRestLateTime() {
+		return kidsRestLateTime;
+	}
+	public void setKidsRestLateTime(String kidsRestLateTime) {
+		this.kidsRestLateTime = kidsRestLateTime;
+	}
+	public String getKidsReason() {
+		return kidsReason;
+	}
+	public void setKidsReason(String kidsReason) {
+		this.kidsReason = kidsReason;
+	}
+	public String getLicenseKindergarten() {
+		return licenseKindergarten;
+	}
+	public void setLicenseKindergarten(String licenseKindergarten) {
+		this.licenseKindergarten = licenseKindergarten;
+	}
+	
+	
+}
