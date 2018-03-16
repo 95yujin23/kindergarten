d920dbffd3b80fd5c27a4038a1b81d3dcc840892
김규원 <kyuwonism@gmail.com>
2018-03-16 15:11:40 +0900

    김규원 - ReportController.java / ReportMapper.xml / ReportService.java 파일 생성

---
+++ kindergarten/src/main/java/com/cafe24/hanboa/report/ReportController.java
@@ -0,0 +1,6 @@
+package com.cafe24.hanboa.report;
+
+public class ReportController {
+	//일지 컨트롤러
+	
+}
---
+++ kindergarten/src/main/java/com/cafe24/hanboa/report/ReportService.java
@@ -0,0 +1,5 @@
+package com.cafe24.hanboa.report;
+
+public class ReportService {
+	//모둔 일지관련 Service
+}
