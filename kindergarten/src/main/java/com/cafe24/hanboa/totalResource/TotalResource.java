a068fe852a344280f25f0b5d7391a2e32df6d0a7
Administrator <Administrator@smart20123>
2018-03-21 11:13:20 +0900

    한보아 - totalResource 관련 파일 소스 등록
    dto 입력완
    mapper 입력미완

--- kindergarten/src/main/java/com/cafe24/hanboa/totalResource/TotalResource.java
+++ kindergarten/src/main/java/com/cafe24/hanboa/totalResource/TotalResource.java
@@ -1,5 +1,32 @@
 package com.cafe24.hanboa.totalResource;
 
 public class TotalResource {
-
+	String totalResourceCd;
+	String resourceGroupNm;
+	String resourceNm;
+	
+	public String getTotalResourceCd() {
+		return totalResourceCd;
+	}
+	public void setTotalResourceCd(String totalResourceCd) {
+		this.totalResourceCd = totalResourceCd;
+	}
+	public String getResourceGroupNm() {
+		return resourceGroupNm;
+	}
+	public void setResourceGroupNm(String resourceGroupNm) {
+		this.resourceGroupNm = resourceGroupNm;
+	}
+	public String getResourceNm() {
+		return resourceNm;
+	}
+	public void setResourceNm(String resourceNm) {
+		this.resourceNm = resourceNm;
+	}
+	
+	@Override
+	public String toString() {
+		return "TotalResource [totalResourceCd=" + totalResourceCd + ", resourceGroupNm=" + resourceGroupNm
+				+ ", resourceNm=" + resourceNm + "]";
+	}
 }
